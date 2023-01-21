060db561aaa62906b18899ca078f23d5900802ae
dudskrla <dudskrla09@gmail.com>
2023-01-22 00:02:20 +0900

    [Fix] 폴더 이름 변경

--- AWS Lambda/[UploadArtwork] lambda_function.py
+++ AWS Lambda/[UploadArtwork] lambda_function.py
@@ -2,6 +2,8 @@ import sys
 import logging
 import pymysql
 import dbinfo
+import json
+
 from urllib.parse import unquote
 
 connection = pymysql.connect(host = dbinfo.db_host, port = dbinfo.db_port,
@@ -10,7 +12,6 @@ connection = pymysql.connect(host = dbinfo.db_host, port = dbinfo.db_port,
 
 def lambda_handler(event, context):
     # user_id=id&artimg_url=url&artwork_name=name&artwork_url=url&public_mode=0
-
     user_id = event['body'].split('user_id=')[1].split('&artimg_url=')[0]
     artimg_url = event['body'].split('artimg_url=')[1].split('&artwork_name=')[0]
     artwork_name = event['body'].split('artwork_name=')[1].split('&artwork_url=')[0]
@@ -25,21 +26,20 @@ def update_db(user_id, artimg_url, artwork_name, artwork_url, public_mode):
     artimg_url = unquote(artimg_url, encoding='utf-8', errors='replace')
     artwork_name = unquote(artwork_name, encoding='utf-8', errors='replace')
     artwork_url = unquote(artwork_url, encoding='utf-8', errors='replace')
-    artwork_url = "[AWS-S3-Bucket]/artwork/" + artwork_url + ".fbx"
-
-    cursor = connection.cursor()
+    
+    artwork_url = "[AWS-S3-BUCKET]/ptc-artwork/" + artwork_url
+    
     query = f"insert into artwork (user_id, artimg_url, artwork_name, artwork_url, public_mode, like_num) values ('{user_id}', '{artimg_url}', '{artwork_name}', '{artwork_url}', {public_mode}, 0)"
+    cursor = connection.cursor()
     cursor.execute(query)
     connection.commit()
     
     if (user_id == "") or (artimg_url == "") or (artwork_name == ""):
-
         return {
             'statusCode': 400,
             'body': "Data value is null" 
         }
     else:
-
         return {
             'statusCode': 200,
             'body': "Updating artlist info complete" 
